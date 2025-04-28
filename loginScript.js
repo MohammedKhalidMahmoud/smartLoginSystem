975b6e7f3a2b49330dc5968e199b3a907aae8526
mkhaled <mohammedkhalidmahmoud@gmail.com>
2025-04-28 14:54:05 +0300

    welcome back

--- loginScript.js
+++ loginScript.js
@@ -1,27 +1,50 @@
 // handle login
 const login_email_field=document.querySelector("#login_email_field");
 const login_password_field=document.querySelector("#login_password_field");
-// console.log(login_email_field);
+const login_button=document.querySelector("#login_btn");
+// console.log(login_button);
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'John Doe',
-      email: 'john@example.com'
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-    window.location.href = 'signup.html';
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
-  
+login_button.addEventListener('click',checkCredentials);
+
+
+function checkCredentials(e){
+  e.preventDefault();
+  let email=login_email_field.value; 
+  let password=login_password_field.value; 
+  // console.log(name);
+  console.log(email);
+  console.log(password);
+  let credentials=JSON.parse(localStorage.getItem("credentials")) || [];
+  console.log(credentials.length);
+  for(let i=0;i<credentials.length;i++){
+    if(credentials[i].email===email && credentials[i].password===password){
+      localStorage.setItem("name", credentials[i].name)
+      window.location.href = "./home.html";
+    }
+      
+    else if(credentials[i].email===email && credentials[i].password!==password){
+      console.log("Password is not correct");
+    }
+    else{
+      console.log("You are not registered");
+    }
+  }
+  // if(validateCredentials(name, email, password)){
+  //   let credentials=[{
+  //     name,
+  //     email,
+  //     password
+  //   },...JSON.parse(localStorage.getItem("credentials")) || []
+  //   ]
+    
+  //   // console.log(storage);
+  //   localStorage.setItem("credentials",JSON.stringify(credentials))
+  // }
+}
+
+// function validateCredentials(name, email, password){
+//   return true;
+// }
 
 
 
--- signupScript.js
+++ signupScript.js
@@ -4,28 +4,38 @@
 const signup_name_field=document.querySelector("#signup_name_field");
 const signup_email_field=document.querySelector("#signup_email_field");
 const signup_password_field=document.querySelector("#signup_password_field");
+const signup_button=document.querySelector("#signup_btn");
 // console.log(login_email_field);
+signup_button.addEventListener('click',saveCredentials);
 
-fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'John Doe',
-      email: 'john@example.com'
-    })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log('Success:', data);
-    window.location.href = 'signin.html';
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
+const name_regex=
+function saveCredentials(e){
+  e.preventDefault();
+  let name=signup_name_field.value; 
+  let email=signup_email_field.value; 
+  let password=signup_password_field.value; 
+  console.log(name);
+  console.log(email);
+  console.log(password);
   
+  if(validateCredentials(name, email, password)){
+    let credentials=[{
+      name,
+      email,
+      password
+    },...JSON.parse(localStorage.getItem("credentials")) || []
+    ]
+    
+    // console.log(storage);
+    localStorage.setItem("credentials",JSON.stringify(credentials))
+  }
+}
 
+function validateCredentials(name, email, password){
+  if(name==='' || email==='' || password===''){
+    console.log
+  }
+}
 
 
 
